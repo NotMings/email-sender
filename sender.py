15e2b07c7501f7326fa4826f6b5c90adcb9e9b7b
不明 <1679143967@qq.com>
2023-06-10 21:30:21 +0800

    使参数可配置

--- sender.py
+++ sender.py
@@ -6,7 +6,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-def send_email(sender, password, server, port, recipient, cc, subject, body, attachment):
+def send_email(sender, password, server, port, connection, recipient, cc, subject, body, attachment):
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = recipient
@@ -22,7 +22,12 @@ def send_email(sender, password, server, port, recipient, cc, subject, body, att
             part.add_header('Content-Disposition', 'attachment; filename= %s' % attachment)
             msg.attach(part)
 
-    server = smtplib.SMTP_SSL(server, port)
+    if connection == 'ssl':
+        server = smtplib.SMTP_SSL(server, port)
+    else:
+        server = smtplib.SMTP(server, port)
+        if connection == 'tls':
+            server.starttls()
     server.login(sender, password)
     server.sendmail(sender, recipient.split(',') + cc.split(','), msg.as_string())
     server.quit()
@@ -30,18 +35,29 @@ def send_email(sender, password, server, port, recipient, cc, subject, body, att
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='mail.conf', help='Path to config file')
-    parser.add_argument('--recipient', required=True, help='Email recipient')
-    parser.add_argument('--cc', default='', help='Email cc')
-    parser.add_argument('--subject', default='', help='Email subject')
-    parser.add_argument('--body', required=True, help='Email body')
+    parser.add_argument('--sender', default=None, help='Email sender')
+    parser.add_argument('--password', default=None, help='Email password')
+    parser.add_argument('--server', default=None, help='Email server')
+    parser.add_argument('--port', default=None, type=int, help='Email port')
+    parser.add_argument('--connection', default=None, help='Email connection type')
+    parser.add_argument('--recipient', default=None, help='Email recipient')
+    parser.add_argument('--cc', default=None, help='Email cc')
+    parser.add_argument('--subject', default=None, help='Email subject')
+    parser.add_argument('--body', default=None, help='Email body')
     parser.add_argument('--attachment', default=None, help='Path to attachment file')
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
     config.read(args.config)
-    sender = config.get('email', 'sender')
-    password = config.get('email', 'password')
-    server = config.get('email', 'server')
-    port = config.getint('email', 'port')
+    sender = args.sender if args.sender else config.get('email', 'sender')
+    password = args.password if args.password else config.get('email', 'password')
+    server = args.server if args.server else config.get('email', 'server')
+    port = args.port if args.port else config.getint('email', 'port')
+    connection = args.connection if args.connection else config.get('email', 'connection')
+    recipient = args.recipient if args.recipient else config.get('config', 'recipient')
+    cc = args.cc if args.cc else config.get('config', 'cc')
+    subject = args.subject if args.subject else config.get('config', 'subject')
+    body = args.body if args.body else config.get('config', 'body')
+    attachment = args.attachment if args.attachment else config.get('config', 'attachment')
 
-    send_email(sender, password, server, port, args.recipient, args.cc, args.subject, args.body, args.attachment)
+    send_email(sender, password, server, port, connection, recipient, cc, subject, body, attachment)
